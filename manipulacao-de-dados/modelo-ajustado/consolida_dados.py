9869ceabf6c241a74e5016a31557f0c49fff9720
TommDaniel <daninho.tomm@gmail.com>
2025-05-22 21:04:15 -0300

    ajust in enviroment.yml

--- manipulacao-de-dados/modelo-ajustado/consolida_dados.py
+++ manipulacao-de-dados/modelo-ajustado/consolida_dados.py
@@ -19,16 +19,15 @@ log_path = os.path.join(LOG_DIR, 'log.txt')
 now = datetime.now()
 
 with open(log_path, 'a', encoding='utf-8') as log:
-	log.write("------Consolida dados ModeloAjustado Excecução iniciada em: " + str(now) + "\n\n")
+    log.write("------Consolida dados ModeloAjustado Excecução iniciada em: " + str(now) + "\n\n")
 
 try:
 #Conecta com o BD
-	con = psycopg2.connect("host='localhost' port='5433' dbname='db_2020' user='postgres' password='12345'")
-	#con = psycopg2.connect("host='localhost' port='5432' dbname='pastagemOutlier' user='postgres' password='123456'")
-	cur = con.cursor()
-		
+    con = psycopg2.connect("host='localhost' port='5432' dbname='db_2020' user='postgres' password='030902'")
+    #con = psycopg2.connect("host='localhost' port='5432' dbname='pastagemOutlier' user='postgres' password='123456'")
+    cur = con.cursor()    
 except:
-	log.write("\nFalha na conexão com o BD!")
+    log.write("\nFalha na conexão com o BD!")
 
 file = open("selectent.sql", 'r')
 sql = " ".join(file.readlines())
@@ -37,13 +36,13 @@ cur.execute(sql)
 con.commit()
 
 try:
-	tdo= cur.fetchall()
+    tdo= cur.fetchall()
 
-	ent = []
-	ant=None
+    ent = []
+    ant=None
 
 except:
-	print("Erro - Nao foi possivel acessar os dados no BD")
+    print("Erro - Nao foi possivel acessar os dados no BD")
 
 file1 = open("selectmstotalD.sql", 'r')
 sql1 = " ".join(file1.readlines())
@@ -52,9 +51,9 @@ cur.execute(sql1)
 con.commit()
 
 try:
-	dados1= cur.fetchall()
+    dados1= cur.fetchall()
 except:
-	print("Erro - Nao foi possivel acessar os dados no BD")
+    print("Erro - Nao foi possivel acessar os dados no BD")
 
 file2 = open("selectmstotalF.sql", 'r')
 sql2 = " ".join(file2.readlines())
@@ -63,9 +62,9 @@ cur.execute(sql2)
 con.commit()
 
 try:
-	dados2= cur.fetchall()
+    dados2= cur.fetchall()
 except:
-	print("Erro - Nao foi possivel acessar os dados no BD")
+    print("Erro - Nao foi possivel acessar os dados no BD")
 
 
 mstotalD = [float(d[0]) for d in dados1]
@@ -78,135 +77,135 @@ print(quartilF)
 
 for x in tdo:
 
-	if (ant == None): 
-		ant=x
-		continue
-	#print(x)
-	if (ant[1] != x[1]) and x[0]=='D':
-		c=x[1]-ant[1]
+    if (ant == None): 
+        ant=x
+        continue
+    #print(x)
+    if (ant[1] != x[1]) and x[0]=='D':
+        c=x[1]-ant[1]
 
-		file = open("selectclima.sql", 'r')
-		sqlclima = " ".join(file.readlines())
+        file = open("selectclima.sql", 'r')
+        sqlclima = " ".join(file.readlines())
 
-		data = (ant[1].isoformat(), x[1].isoformat())
+        data = (ant[1].isoformat(), x[1].isoformat())
 
-		cur.execute(sqlclima, data)
-		con.commit()
+        cur.execute(sqlclima, data)
+        con.commit()
 
-		clima= cur.fetchall()
-		
-		DG = x[3]
-		FG = ant[3]
-		
-		DG = float(DG)
-		FG = float(FG)
-		
-		if DG < quartilD[0] and FG < quartilF[0]:
-			TA = (DG-FG)/(x[1]-ant[1]).days
-		if DG < quartilD[0] and FG > quartilF[0]:
-			TA = (DG-quartilF[0])/(x[1]-ant[1]).days		
-		if DG > quartilD[0] and FG < quartilF[0]:
-			TA = (quartilD[0]-FG)/(x[1]-ant[1]).days
-		if DG > quartilD[0] and FG > quartilF[0]:
-			TA = (quartilD[0]-quartilF[0])/(x[1]-ant[1]).days
-		
-		if x[7] == 1:
-			trat1 = 1 #infestado
-			trat2 = 0 #mirapasto
-		elif x[7] == 2:
-			trat1 = 0
-			trat2 = 1
-		elif x[7] == 3:
-			trat1 = 1
-			trat2 = 0
-		else:
-			trat1 = 0
-			trat2 = 1
-		
-		if TA<0:
-			TA=0
-		
-		if x[3] > quartilD:
-			if ant[3] > quartilF:
-				#presente
-				#ent.append([x[1], (x[1]-ant[1]).days, x[2], ant[2], x[4], ant[3], clima[0][0], clima[0][7],
-				#		   clima[0][14], clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], 
-				#		   clima[0][16], clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], 
-				#		   clima[0][18], clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], 
-				#		   clima[0][20], clima[0][21], clima[0][22], clima[0][23],clima[0][24], clima[0][25], 
-				#		   clima[0][26],clima[0][27], clima[0][28], clima[0][29], x[3]])
-				#futuro
-				ent.append([x[5], x[6], (x[1]-ant[1]).days, ant[2], '%.2f' % quartilF[0], '%.2f' % quartilD[0], '', clima[0][0], clima[0][7], clima[0][14],
-							clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], clima[0][16], 
-							clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], clima[0][18], 
-							clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], clima[0][20], 
-							clima[0][21], clima[0][22], clima[0][23], clima[0][24], clima[0][25], clima[0][26],
-							clima[0][27], clima[0][28], clima[0][29], '%.2f' % TA])
-			else:
-				#presente
-				#ent.append([x[1], (x[1]-ant[1]).days, x[2], ant[2], x[4], ant[3], clima[0][0], clima[0][7],
-				#		   clima[0][14], clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], 
-				#		   clima[0][16], clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], 
-				#		   clima[0][18], clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], 
-				#		   clima[0][20], clima[0][21], clima[0][22], clima[0][23],clima[0][24], clima[0][25], 
-				#		   clima[0][26],clima[0][27], clima[0][28], clima[0][29], x[3]])
-				#futuro
-				ent.append([x[5], x[6], (x[1]-ant[1]).days, ant[2], ant[3], '%.2f' % quartilD[0], '', clima[0][0], clima[0][7], clima[0][14],
-							clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], clima[0][16], 
-							clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], clima[0][18], 
-							clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], clima[0][20], 
-							clima[0][21], clima[0][22], clima[0][23], clima[0][24], clima[0][25], clima[0][26],
-							clima[0][27], clima[0][28], clima[0][29], '%.2f' % TA])				
-		else:
-			if ant[3] > quartilF:
-				#presente
-				#ent.append([x[1], (x[1]-ant[1]).days, x[2], ant[2], x[4], ant[3], clima[0][0], clima[0][7],
-				#		   clima[0][14], clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], 
-				#		   clima[0][16], clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], 
-				#		   clima[0][18], clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], 
-				#		   clima[0][20], clima[0][21], clima[0][22], clima[0][23],clima[0][24], clima[0][25], 
-				#		   clima[0][26],clima[0][27], clima[0][28], clima[0][29], x[3]])
-				#futuro
-				ent.append([x[5], x[6], (x[1]-ant[1]).days, ant[2], '%.2f' % quartilF[0], x[3], '', clima[0][0], clima[0][7], clima[0][14],
-							clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], clima[0][16], 
-							clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], clima[0][18], 
-							clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], clima[0][20], 
-							clima[0][21], clima[0][22], clima[0][23], clima[0][24], clima[0][25], clima[0][26],
-							clima[0][27], clima[0][28], clima[0][29], '%.2f' % TA])
-			else:
-				#presente
-				#ent.append([x[1], (x[1]-ant[1]).days, x[2], ant[2], x[4], ant[3], clima[0][0], clima[0][7],
-				#		   clima[0][14], clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], 
-				#		   clima[0][16], clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], 
-				#		   clima[0][18], clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], 
-				#		   clima[0][20], clima[0][21], clima[0][22], clima[0][23],clima[0][24], clima[0][25], 
-				#		   clima[0][26],clima[0][27], clima[0][28], clima[0][29], x[3]])
-				#futuro
-				ent.append([x[5], x[6], (x[1]-ant[1]).days, ant[2], ant[3], x[3], '', clima[0][0], clima[0][7], clima[0][14],
-							clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], clima[0][16], 
-							clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], clima[0][18], 
-							clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], clima[0][20], 
-							clima[0][21], clima[0][22], clima[0][23], clima[0][24], clima[0][25], clima[0][26],
-							clima[0][27], clima[0][28], clima[0][29], '%.2f' % TA])
-		
-	ant=x
-	
+        clima= cur.fetchall()
+        
+        DG = x[3]
+        FG = ant[3]
+        
+        DG = float(DG)
+        FG = float(FG)
+        
+        if DG < quartilD[0] and FG < quartilF[0]:
+            TA = (DG-FG)/(x[1]-ant[1]).days
+        if DG < quartilD[0] and FG > quartilF[0]:
+            TA = (DG-quartilF[0])/(x[1]-ant[1]).days        
+        if DG > quartilD[0] and FG < quartilF[0]:
+            TA = (quartilD[0]-FG)/(x[1]-ant[1]).days
+        if DG > quartilD[0] and FG > quartilF[0]:
+            TA = (quartilD[0]-quartilF[0])/(x[1]-ant[1]).days
+        
+        if x[7] == 1:
+            trat1 = 1 #infestado
+            trat2 = 0 #mirapasto
+        elif x[7] == 2:
+            trat1 = 0
+            trat2 = 1
+        elif x[7] == 3:
+            trat1 = 1
+            trat2 = 0
+        else:
+            trat1 = 0
+            trat2 = 1
+        
+        if TA<0:
+            TA=0
+        
+        if x[3] > quartilD:
+            if ant[3] > quartilF:
+                #presente
+                #ent.append([x[1], (x[1]-ant[1]).days, x[2], ant[2], x[4], ant[3], clima[0][0], clima[0][7],
+                #           clima[0][14], clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], 
+                #           clima[0][16], clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], 
+                #           clima[0][18], clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], 
+                #           clima[0][20], clima[0][21], clima[0][22], clima[0][23],clima[0][24], clima[0][25], 
+                #           clima[0][26],clima[0][27], clima[0][28], clima[0][29], x[3]])
+                #futuro
+                ent.append([x[5], x[6], (x[1]-ant[1]).days, ant[2], '%.2f' % quartilF[0], '%.2f' % quartilD[0], '', clima[0][0], clima[0][7], clima[0][14],
+                            clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], clima[0][16], 
+                            clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], clima[0][18], 
+                            clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], clima[0][20], 
+                            clima[0][21], clima[0][22], clima[0][23], clima[0][24], clima[0][25], clima[0][26],
+                            clima[0][27], clima[0][28], clima[0][29], '%.2f' % TA])
+            else:
+                #presente
+                #ent.append([x[1], (x[1]-ant[1]).days, x[2], ant[2], x[4], ant[3], clima[0][0], clima[0][7],
+                #           clima[0][14], clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], 
+                #           clima[0][16], clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], 
+                #           clima[0][18], clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], 
+                #           clima[0][20], clima[0][21], clima[0][22], clima[0][23],clima[0][24], clima[0][25], 
+                #           clima[0][26],clima[0][27], clima[0][28], clima[0][29], x[3]])
+                #futuro
+                ent.append([x[5], x[6], (x[1]-ant[1]).days, ant[2], ant[3], '%.2f' % quartilD[0], '', clima[0][0], clima[0][7], clima[0][14],
+                            clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], clima[0][16], 
+                            clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], clima[0][18], 
+                            clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], clima[0][20], 
+                            clima[0][21], clima[0][22], clima[0][23], clima[0][24], clima[0][25], clima[0][26],
+                            clima[0][27], clima[0][28], clima[0][29], '%.2f' % TA])                
+        else:
+            if ant[3] > quartilF:
+                #presente
+                #ent.append([x[1], (x[1]-ant[1]).days, x[2], ant[2], x[4], ant[3], clima[0][0], clima[0][7],
+                #           clima[0][14], clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], 
+                #           clima[0][16], clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], 
+                #           clima[0][18], clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], 
+                #           clima[0][20], clima[0][21], clima[0][22], clima[0][23],clima[0][24], clima[0][25], 
+                #           clima[0][26],clima[0][27], clima[0][28], clima[0][29], x[3]])
+                #futuro
+                ent.append([x[5], x[6], (x[1]-ant[1]).days, ant[2], '%.2f' % quartilF[0], x[3], '', clima[0][0], clima[0][7], clima[0][14],
+                            clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], clima[0][16], 
+                            clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], clima[0][18], 
+                            clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], clima[0][20], 
+                            clima[0][21], clima[0][22], clima[0][23], clima[0][24], clima[0][25], clima[0][26],
+                            clima[0][27], clima[0][28], clima[0][29], '%.2f' % TA])
+            else:
+                #presente
+                #ent.append([x[1], (x[1]-ant[1]).days, x[2], ant[2], x[4], ant[3], clima[0][0], clima[0][7],
+                #           clima[0][14], clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], 
+                #           clima[0][16], clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], 
+                #           clima[0][18], clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], 
+                #           clima[0][20], clima[0][21], clima[0][22], clima[0][23],clima[0][24], clima[0][25], 
+                #           clima[0][26],clima[0][27], clima[0][28], clima[0][29], x[3]])
+                #futuro
+                ent.append([x[5], x[6], (x[1]-ant[1]).days, ant[2], ant[3], x[3], '', clima[0][0], clima[0][7], clima[0][14],
+                            clima[0][1], clima[0][8], clima[0][15], clima[0][2], clima[0][9], clima[0][16], 
+                            clima[0][3], clima[0][10], clima[0][17], clima[0][4], clima[0][11], clima[0][18], 
+                            clima[0][5], clima[0][12], clima[0][19], clima[0][6], clima[0][13], clima[0][20], 
+                            clima[0][21], clima[0][22], clima[0][23], clima[0][24], clima[0][25], clima[0][26],
+                            clima[0][27], clima[0][28], clima[0][29], '%.2f' % TA])
+        
+    ant=x
+    
 
 import csv
 
 #presente
 #header = ['data', 'numerodias', 'alturamedia', 'alturamediaanterior', 'pcanonni', 'mstotalanterior', 
-#		  'tmin', 'dptmin', 'vartmin', 'tmed', 'dptmed', 'vartmed', 'tmax', 'dptmax', 'vartmax', 
-#		  'umidade', 'dpumidade', 'varumidade', 'velocidadevento', 'dpvelocidadevento', 'varvelovidadevento', 
-#		  'radiacaosolar', 'dpradiacaosolar', 'varradiacaosolar', 'chuva', 'dpchuva', 'varchuva', 'somatermica', 
-#		  'dpsomatermica', 'varsomatermica', 'def', 'dpdef', 'vardef', 'exc', 'dpexc', 'varexc', 'taxaacumulo']
+#          'tmin', 'dptmin', 'vartmin', 'tmed', 'dptmed', 'vartmed', 'tmax', 'dptmax', 'vartmax', 
+#          'umidade', 'dpumidade', 'varumidade', 'velocidadevento', 'dpvelocidadevento', 'varvelovidadevento', 
+#          'radiacaosolar', 'dpradiacaosolar', 'varradiacaosolar', 'chuva', 'dpchuva', 'varchuva', 'somatermica', 
+#          'dpsomatermica', 'varsomatermica', 'def', 'dpdef', 'vardef', 'exc', 'dpexc', 'varexc', 'taxaacumulo']
 
 #futuro
 header = ['mes', 'ano', 'numerodias', 'alturamediaanterior', 'mstotalanterior', 'mstotal', 'CA', 'tmin', 'dptmin', 'vartmin', 'tmed', 
-		   'dptmed', 'vartmed', 'tmax', 'dptmax', 'vartmax', 'umidade', 'dpumidade', 'varumidade', 'velocidadevento', 
-		   'dpvelocidadevento', 'varvelovidadevento', 'radiacaosolar', 'dpradiacaosolar', 'varradiacaosolar', 'chuva', 
-		   'dpchuva', 'varchuva', 'somatermica', 'dpsomatermica', 'varsomatermica', 'def', 'dpdef', 'vardef', 'exc', 
-		   'dpexc', 'varexc', 'TA']
+           'dptmed', 'vartmed', 'tmax', 'dptmax', 'vartmax', 'umidade', 'dpumidade', 'varumidade', 'velocidadevento', 
+           'dpvelocidadevento', 'varvelovidadevento', 'radiacaosolar', 'dpradiacaosolar', 'varradiacaosolar', 'chuva', 
+           'dpchuva', 'varchuva', 'somatermica', 'dpsomatermica', 'varsomatermica', 'def', 'dpdef', 'vardef', 'exc', 
+           'dpexc', 'varexc', 'TA']
 
 
 #REVERSE
@@ -215,11 +214,11 @@ header = ['mes', 'ano', 'numerodias', 'alturamediaanterior', 'mstotalanterior', 
 csv_path = os.path.join(DATA_DIR, 'e1_Treino.csv')
 
 with open(csv_path, 'w', newline='') as myfile:
-	wr = csv.writer(myfile, quoting=csv.QUOTE_ALL,	delimiter=';')
-	wr.writerow(header)
-	wr.writerows(ent)
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL,    delimiter=';')
+    wr.writerow(header)
+    wr.writerows(ent)
 
 now = datetime.now()
 
 with open(log_path, 'a', encoding='utf-8') as log:
-	log.write("------Consolida dados ModeloAjustado Excecução iniciada em: " + str(now) + "\n\n")
+    log.write("------Consolida dados ModeloAjustado Excecução iniciada em: " + str(now) + "\n\n")
